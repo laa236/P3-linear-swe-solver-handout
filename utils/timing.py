fc19f3072ac0d8d39a4f28d2198e93a7639514c4
Evan Vera <74469557+devovevo@users.noreply.github.com>
2024-11-03 01:24:47 -0400

    Update timing.py

--- utils/timing.py
+++ utils/timing.py
@@ -12,7 +12,7 @@ num_iters = 10000
 serial_sizes = [ 100, 250, 500, 750, 1000 ]
 gpu_sizes = [ 100, 500, 1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000 ]
 
-mpi_size_strong = 1024
+mpi_size_strong = 2048
 mpi_sizes_weak = [ 512, 1024, 2048, 4096 ]
 
 mpi_tasks_per_node = 128
